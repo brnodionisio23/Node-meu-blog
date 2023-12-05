1318ec364fd74f6f76f1ce38092826f889e4577e
Breno Dionisio <brnodionisio23@gmail.com>
2023-12-05 14:08:55 -0300

    Configurando EJS + Bootstrap

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 8080;
 
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}`)
 })
