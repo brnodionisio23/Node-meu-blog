b8ad335e30195c36c4e0de1549e31110096b1845
Breno Dionisio <brnodionisio23@gmail.com>
2023-12-05 14:14:50 -0300

    Configurando body-parser

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 const port = 8080;
 
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.render('index')
