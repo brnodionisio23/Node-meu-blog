4ea26714fb278eab18e5576fb5f572ab3b7ca06b
Breno Dionisio <brnodionisio23@gmail.com>
2023-12-05 13:47:54 -0300

    Definindo template engine para EJS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'ejs');
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}`)
