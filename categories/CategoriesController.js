2335be6fac2804f60d56f5071b6936392adc7891
Breno Dionisio <brnodionisio23@gmail.com>
2023-12-27 13:04:07 -0300

    Criando area de edição de categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Category = require('./Category');
 const slugify = require('slugify');
+const { where } = require('sequelize');
 
 router.get('/admin/category', (req, res) => {
     Category.findAll().then(category => {
@@ -23,6 +24,17 @@ router.post("/admin/category/saveCategory", (req, res) => {
     })
 })
 
+router.get('/admin/category/editCategory/:id', (req, res) => {
+    let id = req.params.id;
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render('admin/category/edit', { category: category })
+        } else {
+            res.redirect('/admin/category')
+        }
+    })
+})
+
 router.post('/admin/category/deleteCategory', (req, res) => {
     let id = req.body.id;
     Category.destroy({
@@ -33,6 +45,19 @@ router.post('/admin/category/deleteCategory', (req, res) => {
         res.redirect('/admin/category');
     }).catch(err => {
         console.log(err)
+    })
+})
+
+router.post('/admin/category/update', (req, res) => {
+    let id = req.body.id;
+    let title = req.body.inpCategory;
+
+    Category.update({ title: title, slug: slugify(title) }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect('/admin/category');
     })
 })
 
