6eaf2c9022cca00166244f80fa60c0fbd1e6e16a
Breno Dionisio <brnodionisio23@gmail.com>
2023-12-28 14:24:31 -0300

    Dropdown de categorias

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const Category = require('../categories/Category');
 const router = express.Router();
 
 router.get("/admin/article/new", (req, res) => {
-    res.render('admin/article/new');
+    Category.findAll().then(categories => {
+        res.render('admin/article/new', { categories: categories });
+    })
 })
 
 module.exports = router;
